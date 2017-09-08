8e2d4a73b550f5d5041e42f631017f1e5b423e06
AJRedDevil <ajan.shresh@gmail.com>
2017-09-08 11:13:21 +0545

    Application state sync with browser history

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -27,6 +27,15 @@ export class Router extends Component {
             linkHandler: this.handleLinkClick
         }
     }
+
+    componentDidMount() {
+        window.onpopstate = () => {
+            this.setState({
+                route: getCurrentPath()
+            });
+        }
+    }
+    
     
     render() {
         return (
