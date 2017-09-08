8cfeadd00611b5b915e9e27e823f329d76502336
AJRedDevil <ajan.shresh@gmail.com>
2017-09-08 09:52:09 +0545

    Pip function to composite functions

--- src/lib/utils.jsx
+++ src/lib/utils.jsx
@@ -1,1 +1,5 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args) // argument into array and then later spread array.
+
+const _pipe = (f, g) => (...args) => g(f(...args));
+
+export const pipe = (...fns) => fns.reduce(_pipe); // without args in reduce it will take first two arguments and pass it on to the function
